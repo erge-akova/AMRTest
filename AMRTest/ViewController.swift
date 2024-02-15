e2f4909d6fa6f0cfc01e85195c2dd2eee2cd600a
Erge AKOVA <117632508+erge-akova@users.noreply.github.com>
2024-02-15 21:43:56 +0300

    AMRTest

--- AMRTest/ViewController.swift
+++ AMRTest/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AMRSDK
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AMRBannerDelegate {
 
+    @IBOutlet var native250: UIView!
+    var native: AMRBanner!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        native = AMRBanner.init(forZoneId:"ccdea0b0-3629-49b2-a205-c72627d8229a")
+        native.delegate = self
+        native.customNativeSize = CGSize(width: 300.0, height: 250.0)
+        native.customeNativeXibName = "AMRNativeAdBaseViewAlt250"
+        native.load()   
     }
-
-
+    
+    @IBAction func load(_ sender: Any) {
+        native.load()
+    }
+    
+    // MARK: Banner
+    func didReceive(_ banner: AMRBanner!) {
+        print("<AMRTest> [Native] didReceive")
+            self.native250.addSubview(banner.bannerView)
+    }
+    
+    func didFail(toReceive banner: AMRBanner!, error: AMRError!) {
+        print("<AMRTest> [Native] didFail \(String(describing: error.errorDescription))")
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AMRTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AMRTest
+
+  pod 'AMRSDK', '~> 1.5'
+  pod 'AMRAdapterYandex', '~>6.1'
+
+end
